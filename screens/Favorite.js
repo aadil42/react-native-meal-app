81139d8fa7c80010563f78ab7da240e5e2ba4f9f
aadil42 <adilkelawala@gmail.com>
2023-10-23 12:22:04 +0530

    display favorite meals on favorite screen

--- screens/Favorite.js
+++ screens/Favorite.js
@@ -1,20 +1,35 @@
-import { Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, FlatList} from "react-native";
+import { useContext } from "react";
+import { FavoriteContext } from "../store/context/favorite-context";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+const Favorite = ({ navigation, route }) => {
 
-const Favorite = () => {
+    const favContext = useContext(FavoriteContext);
+    const list = MEALS.filter((meal) => {
+        if(favContext.favoriteIds.includes(meal.id)) return meal;
+    });
+
+
+    const renderer = (itemData) => {
+        const item = {...itemData.item, navigation};
+        return <MealItem {...item} />
+    }
+
     return (
         <View style={styles.container}>
-            <Text>
-                This is Favorite screen
-            </Text>
+            <FlatList 
+            data={list}
+            renderItem={renderer}
+            keyExtractor={(item) => item.id}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        paddingTop: 20
     }
 });
 
